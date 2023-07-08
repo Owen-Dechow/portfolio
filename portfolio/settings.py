5873e27cc76ec351e8ad5e123d6b32717ce1ec9e
Owen Dechow <owen,dechow@gmail.com>
2023-07-07 20:40:19 -0400

    Update static file settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,12 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "staticfiles/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

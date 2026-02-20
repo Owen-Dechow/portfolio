1b96ed0550ee1d8d8e2e582486365a6507904821
Owen Dechow <owen.dechow@gmail.com>
2026-02-20 17:32:48 -0500

    Resume link

--- base/views.py
+++ base/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def homepage(request: HttpRequest) -> HttpResponse:
     return render(request, "base/homepage.html")
